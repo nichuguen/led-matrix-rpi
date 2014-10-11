ef3def73b6c7fcb8acd04b071c0d6d720cfecfe3
Nicolas Huguenin <nicolas.huguenin@he-arc.ch>
2014-10-11 18:19:23 +0200

    More readable main function code

--- showtext.py
+++ showtext.py
@@ -1,5 +1,6 @@
 import os
 import time
+import signal
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
@@ -23,8 +24,9 @@ def showOnLEDMatrix(textsTuple):
 	#"/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf" size 14
 	all_text = ""
 	for text_color_pair in text:
-		t = text_color_pair[0]
-		all_text = all_text + t
+		if text_color_pair is not None:
+			t = text_color_pair[0]
+			all_text = all_text + t
 
 	print(all_text)
 	width, ignore = font.getsize(all_text)
@@ -36,19 +38,19 @@ def showOnLEDMatrix(textsTuple):
 
 	x = 0;
 	for text_color_pair in text:
-		t = text_color_pair[0]
-		c = text_color_pair[1]
-		print("t=" + t + " " + str(c) + " " + str(x))
-		draw.text((x, 0), t, c, font=font)
-		x = x + font.getsize(t)[0]
+		if text_color_pair is not None:
+			t = text_color_pair[0]
+			c = text_color_pair[1]
+			print("t=" + t + " " + str(c) + " " + str(x))
+			draw.text((x, 0), t, c, font=font)
+			x = x + font.getsize(t)[0]
 
 	im.save("test.ppm")
 	return os.spawnl( os.P_NOWAIT, "./led-matrix", "./led-matrix", "1","test.ppm")
 
 if __name__ == "__main__":
-	text = (createTextColor("QWERTZUIOPASDFGHJKLYXCVBNM", (255, 0, 0)), createTextColor("qwertzuiopasdfghjklyxcvbnm", (0, 255, 0)), createTextColor("1234567890", (0, 0, 255)))
+	text = (createTextColor("QWERTZUIOPASDFGHJKLYXCVBNM", (255, 0, 0)), None)
 	pid = showOnLEDMatrix(text)
-	time.sleep(2)
-	strcommand = "kill -9 %d" %pid
-	os.system(strcommand)
+	time.sleep(10)
+	os.kill(pid, signal.SIGKILL)
 	clearLEDMatrix()
