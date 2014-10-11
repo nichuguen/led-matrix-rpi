c158bf06da279fed612386c9f28cf71ee8802cec
Nicolas Huguenin <nicolas.huguenin@he-arc.ch>
2014-10-11 19:01:05 +0200

    added led matrix support

--- ledserver.py
+++ ledserver.py
@@ -1,4 +1,7 @@
 import socket
+import showtext
+import os
+import signal
 
 IP = ""
 PORT = 6666
@@ -7,49 +10,60 @@ FILE_NAME = "led-server.log"
 MSG_SIZE = 1024
 
 f = open("./"+FILE_NAME, 'a')
+pid = 0
 
 def decode(receivedString):
-    myList = receivedString.split(";")
-    if len(myList) == 3:
-        myList[0] = "Song: "+myList[0]
-        myList[1] = "Artist: "+myList[1]
-        myList[2] = "Album: "+myList[2]
-    for myString in myList:
-        print(myString)
-    print("")
+	global pid
+	if pid != 0:
+		os.kill(pid, signal.SIGKILL)
+	showtext.clearLEDMatrix()
+	myList = receivedString.split(";")
+	if len(myList) == 3:
+		myList[0] = "Song: "+myList[0]
+		myList[1] = "Artist: "+myList[1]
+		myList[2] = "Album: "+myList[2]
+	myTupleList = list()
+	for myString in myList:
+		myTupleList.append(showtext.createTextColor(myString, (255, 0,0)))
+		print( myString )
+	pid = showtext.showOnLEDMatrix(tuple(myTupleList))
+	print("")
 
 def save(receivedString):
-    f.write(receivedString+'\n')
+	f.write(receivedString+'\n')
 
 clientSocket = None
 
 try:
-    print("ledserver.py started")
-    #create an INET, STREAMing socket
-    serverSocket = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM)
-    #bind the socket to a public host,
-    # and a well-known port
-    serverSocket.bind((IP, PORT))
+	print("ledserver.py started")
+	#create an INET, STREAMing socket
+	serverSocket = socket.socket(
+		socket.AF_INET, socket.SOCK_STREAM)
+	#bind the socket to a public host,
+	# and a well-known port
+	serverSocket.bind((IP, PORT))
 
-    print("Listening")
-    #become a server socket
-    serverSocket.listen(1)
+	print("Listening")
+	#become a server socket
+	serverSocket.listen(1)
 
-    (clientSocket, address) = serverSocket.accept()
-    print("Accepted")
-    while 1:
-        clientRequest = clientSocket.recv(MSG_SIZE)
-        if not clientRequest:
-            break
-        decode(clientRequest)
-        save(clientRequest)
-    clientSocket.close()
-    f.close()
+	(clientSocket, address) = serverSocket.accept()
+	print("Accepted")
+	while 1:
+		clientRequest = clientSocket.recv(MSG_SIZE)
+		if not clientRequest:
+			break
+		decode(clientRequest)
+		save(clientRequest)
+	clientSocket.close()
+	f.close()
 
 except KeyboardInterrupt:
-    if clientSocket:
-        clientSocket.close()
-    if f:
-        f.close()
-    print("Closing")
+	if clientSocket:
+		clientSocket.close()
+	if f:
+		f.close()
+	print("Closing")
+if pid != 0:
+	os.kill(pid, signal.SIGKILL)
+showtext.clearLEDMatrix()
