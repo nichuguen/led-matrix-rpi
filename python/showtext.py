1d075239577e23f1340a4b694e3f8c52efac26e3
Nicolas Huguenin <nicolas.huguenin@he-arc.ch>
2014-10-30 14:39:48 +0100

    dunno what changed

--- python/showtext.py
+++ python/showtext.py
@@ -9,47 +9,51 @@ from PIL import Image
 from PIL import ImageDraw
 
 def createTextColor( text, color):
-	'''returns a tuple containing the text and the color. the color attribute must be a tuple: (R, G, B)'''
-	return (text, color)
+    '''returns a tuple containing the text and the color. the color attribute must be a tuple: (R, G, B)'''
+    return (text, color)
 
 
 def clearLEDMatrix():
-	'''clears the LED matrix'''
-	os.system(localconstants.clear)
+    '''clears the LED matrix'''
+    os.system(localconstants.clear)
 
 def showOnLEDMatrix(textsTuple):
-	'''shows the texts on the LED matrix 
-	the text must be a tuple of (text, color)
-	returns the PID of the process displaying the text'''
-	text = textsTuple
-	font = ImageFont.truetype("/usr/share/fonts/truetype/droid/DroidSans.ttf", 14)
-	#/fonts/truetype/droid/DroidSans.ttf
-	#"/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf" size 14
-	all_text = ""
-	for text_color_pair in text:
-		if text_color_pair is not None:
-			t = text_color_pair[0]
-			all_text = all_text + t
+    '''shows the texts on the LED matrix 
+    the text must be a tuple of (text, color)
+    returns the PID of the process displaying the text'''
+    text = textsTuple
+    font = ImageFont.truetype("/usr/share/fonts/truetype/droid/DroidSans.ttf", 14)
+    #/fonts/truetype/droid/DroidSans.ttf
+    #"/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf" size 14
+    #NO ONE FUCKS WITH FUCKING EMPTY SPACE
+    '''if text[0][0] is not None: #MAYBE SOMEONE FUCKS WITH THEM
+        text[0][0] = " " + text[0][0]'''
+    all_text = ''
+    for text_color_pair in text:
+        if text_color_pair is not None:
+            t = text_color_pair[0]
+            all_text = all_text + t
 
-	print(all_text)
-	width, ignore = font.getsize(all_text)
-	print(width)
+    print(all_text)
+    width, ignore = font.getsize(all_text)
+    print(width)
 
 
-	im = Image.new("RGB", (width + 30, 16), "black")
-	draw = ImageDraw.Draw(im)
+    im = Image.new("RGB", (width + 30, 16), "black")
+    draw = ImageDraw.Draw(im)
 
-	x = 0;
-	for text_color_pair in text:
-		if text_color_pair is not None:
-			t = text_color_pair[0]
-			c = text_color_pair[1]
-			print("t=" + t + " " + str(c) + " " + str(x))
-			draw.text((x, 0), t, c, font=font)
-			x = x + font.getsize(t)[0]
+    x = 0;
+    for text_color_pair in text:
+        if text_color_pair is not None:
+            t = text_color_pair[0]
+            if x == 0:
+                t = "" + t
+            c = text_color_pair[1]
+            draw.text((x, 0), t, c, font=font)
+            x = x + font.getsize(t)[0]
 
-	im.save("test.ppm")
-	return os.spawnl( os.P_NOWAIT, localconstants.ledmatrix, localconstants.ledmatrix, "1","test.ppm")
+    im.save("test.ppm")
+    return os.spawnl( os.P_NOWAIT, localconstants.ledmatrix, localconstants.ledmatrix, "1","test.ppm")
     
 def encodeAndPrint(args):
     usage = """
@@ -63,17 +67,20 @@ def encodeAndPrint(args):
         text = (createTextColor(textString, (colR, colG, colB)), None)
         pid = showOnLEDMatrix(text)
         print "{ PID: %d }" % pid
-    except:
+        return pid
+    except Exception as e:
+        print e
         print usage
+        return None
         
-def killclearpid(pid):
+def killClearPid(pid):
     usage = """
         killclear <pid>: kills the process <pid> and clears the board
         """
     os.kill(pid, signal.SIGKILL)
     clearLEDMatrix()
     
-def killclearargs(args):
+def killClearArgs(args):
     usage = """
         killclear <pid>: kills the process <pid> and clears the board
         """
@@ -81,7 +88,7 @@ def killclearargs(args):
         pid = 0
         try:
             pid = int(args[2])
-            killclearpid(pid)
+            killClearPid(pid)
         except:
             print(usage)
                 
@@ -106,6 +113,6 @@ if __name__ == "__main__":
         else:
             print (usage)
     elif len(sys.argv) == 3:
-        killclearargs(sys.argv)
+        killClearArgs(sys.argv)
     else:
         default()
