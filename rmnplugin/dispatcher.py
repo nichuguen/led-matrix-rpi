9571284e539c6acead779ae46906f09408cf4e82
Karim Luy <karim.luy@he-arc.ch>
2015-09-24 01:59:29 +0200

    added requests

--- rmnplugin/dispatcher.py
+++ rmnplugin/dispatcher.py
@@ -3,17 +3,26 @@ from localconstants import list_ips as list_ips
 import requests
 
 class Dispatcher:
-	
 	list_addresses_raw = [ ip + ":5000/" for ip in list_ips ]
 	
 	@staticmethod
 	def send_song(song):
-		pass
+		payload = dict(song)
+		payload['type'] = "song"
+		Dispatcher.broadcast_to_pis("", payload)
 		
 	@staticmethod
 	def send_stop():
-		pass
+		payload = {'type' : 'stop'}
+		Dispatcher.broadcast_to_pis("", payload)
 		
 	@staticmethod
 	def send_message(message):
-		pass
+		payload = {'type' : 'stop', 'message' : message}
+		Dispatcher.broadcast_to_pis("", payload)
+		
+	@staticmethod
+	def broadcast_to_pis(address_postfix, data):
+		for address in Dispatcher.list_addresses_raw:
+			requests.post(address+address_postfix, data=data)
+		
