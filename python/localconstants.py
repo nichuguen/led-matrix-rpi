9afc9ceb33bf2b5191f1721d4f3af2783edf77b9
Karim Luy <karim.luy@he-arc.ch>
2015-09-23 20:55:30 +0200

    updated paths

--- python/localconstants.py
+++ python/localconstants.py
@@ -1,3 +1,3 @@
 pathprog = "/home/pi/led-matrix-rpi/c"
-clear = pathprog + "/test-clear"
-ledmatrix = pathprog + "/led-matrix"
+clear = pathprog + "/test-clear.run"
+ledmatrix = pathprog + "/led-matrix.run"
