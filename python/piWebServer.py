2c3ddcfd3c74fa1e9a47b10d6e786669c0f3295f
Karim Luy <karim.luy@he-arc.ch>
2015-09-24 11:24:37 +0200

    added webUI

--- python/piWebServer.py
+++ python/piWebServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for, render_template
 import showtext
 import os
 import signal
@@ -32,36 +32,36 @@ def clear_led():
     showtext.clearLEDMatrix()
 
 @app.route("/", methods=['GET', 'POST'])
-def serv():
-    dict_return = {'type': None, 'result': False}
+def home():
     if request.method == 'GET':
-        return "<h1>NATHING TO SEE HERE BUT IT WERKS</h1>"
+        return render_template("home.html", content="yolo content")
     elif request.method == 'POST':
-        message_type = request.form.get('type', None)
-        if message_type == "song":
-            dict_return['type'] = "song"
-            artist = request.form.get('artist', None)
-            album = request.form.get('album', None)
-            title = request.form.get('title', None)
-            if artist is not None and album is not None and title is not None:
-                clear_led()
-                update_led(artist, album, title)
-                dict_return['result'] = True
-
-        elif message_type == "stop":
-            # clears led
-            clear_led()
-            dict_return['type'] = "stop"
-            dict_return['result'] = True
-        elif message_type == "message":
-            # display message on led
-            global color, pid
-            message = request.form.get('message', None)
-            clear_led()
-            pid = showtext.showOnLEDMatrix(message, color['message'])
-            dict_return['type'] = "message"
-            dict_return['result'] = True
-        return jsonify(**dict_return)
+		dict_return = {'type': None, 'result': False}
+		message_type = request.form.get('type', None)
+		if message_type == "song":
+			dict_return['type'] = "song"
+			artist = request.form.get('artist', None)
+			album = request.form.get('album', None)
+			title = request.form.get('title', None)
+			if artist is not None and album is not None and title is not None:
+				clear_led()
+				update_led(artist, album, title)
+				dict_return['result'] = True
+		elif message_type == "stop":
+			# clears led
+			dict_return['type'] = "stop"
+			clear_led()
+			dict_return['result'] = True
+		elif message_type == "message":
+			# display message on led
+			global color, pid
+			dict_return['type'] = "message"
+			message = request.form.get('message', None)
+			clear_led()
+			if message is not None:
+				pid = showtext.showOnLEDMatrix([(message, color['message'])])
+				dict_return['result'] = True
+		return jsonify(**dict_return)
 
 
 if __name__ == "__main__":
